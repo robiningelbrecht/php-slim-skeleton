4ed7c37b21aaf744a4b38d215a980cdebe17d821
robin <robin@baldwin.be>
2022-08-06 11:01:09 +0200

    Comments

--- config/compiler-passes.php
+++ config/compiler-passes.php
@@ -5,6 +5,14 @@ use App\Infrastructure\Console\ConsoleCommandCompilerPass;
 use App\Infrastructure\CQRS\CommandHandler\CommandHandlerCompilerPass;
 use App\Infrastructure\Eventing\EventListener\EventListenerCompilerPass;
 
+// Compiler passes give you an opportunity to manipulate other service
+// definitions that have been registered with the service container.
+// It's a mechanism copied from Symfony:
+// https://symfony.com/doc/current/service_container/compiler_passes.html
+//
+// The existing compiler passes are mainly used to auto discover
+// classes tagged with a class attribute.
+
 return [
     // Compiler pass to auto discover console commands.
     new ConsoleCommandCompilerPass(),
