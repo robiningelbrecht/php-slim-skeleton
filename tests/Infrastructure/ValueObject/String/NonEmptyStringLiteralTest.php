b15ac79d0a90975a1784ed58d8b9bb71559d8188
robin <robin@baldwin.be>
2022-08-06 18:02:29 +0200

    More unit tests

---
+++ tests/Infrastructure/ValueObject/String/NonEmptyStringLiteralTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Tests\Infrastructure\ValueObject\String;
+
+use PHPUnit\Framework\TestCase;
+
+class NonEmptyStringLiteralTest extends TestCase
+{
+    public function testFromString(): void
+    {
+        $this->assertEquals('test', (string) TestNonEmptyStringLiteral::fromString('test'));
+        $this->assertEquals('test', (string) TestNonEmptyStringLiteral::fromOptionalString('test'));
+        $this->assertNull(TestNonEmptyStringLiteral::fromOptionalString());
+        $this->assertNull(TestNonEmptyStringLiteral::fromOptionalString(''));
+    }
+
+    public function testItShouldThrowWhenEmpty(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('App\Tests\Infrastructure\ValueObject\String\TestNonEmptyStringLiteral can not be empty');
+
+        TestNonEmptyStringLiteral::fromString('');
+    }
+}
