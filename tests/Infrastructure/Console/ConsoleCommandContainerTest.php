8b68005705914ebb882d031b0a9dc6347a8de715
robin <robin@baldwin.be>
2022-08-09 08:10:47 +0200

    CommandHandlerCompilerPassTest

---
+++ tests/Infrastructure/Console/ConsoleCommandContainerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Tests\Infrastructure\Console;
+
+use App\Infrastructure\Console\ConsoleCommandContainer;
+use PHPUnit\Framework\TestCase;
+
+class ConsoleCommandContainerTest extends TestCase
+{
+
+}
