f01bb4ab0970b6064b260f5ce34f52bbbccd01e1
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2022-08-11 19:13:51 +0200

    example remove script

--- bin/example-remove.sh
+++ bin/example-remove.sh
@@ -19,6 +19,19 @@ rm -Rf tests/Controller/*
 rm -Rf tests/Domain/ReadModel/*
 rm -Rf tests/Domain/WriteModel/*
 
+# Update routes.php
+printf '
+<?php
+
+  use Slim\App;
+  use Slim\Handlers\Strategies\RequestResponseArgs;
+
+  return function (App $app) {
+      // Set default route strategy.
+      $routeCollector = $app->getRouteCollector();
+      $routeCollector->setDefaultInvocationStrategy(new RequestResponseArgs());
+  };' > config/routes.php
+
 
 echo "All examples related files are deleted.
 Do not forget to update 'docker/mysql/mysql.databases.sql' to create the database(s) you need"
