f1ce5d71a76f4c79a1a132fb1b58ff4af18fd82c
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2022-08-11 18:55:27 +0200

    Remove script

--- bin/example-remove.sh
+++ bin/example-remove.sh
@@ -2,20 +2,22 @@
 
 # @todo: clear routes file.
 # Public folder.
-rm -Rf ../public/favicon.png
-rm -Rf ../public/style.css
+rm -Rf public/favicon.png
+rm -Rf public/style.css
+# Migrations folder.
+rm -Rf migrations/*
 # Src folder.
-rm -Rf ../src/Console/Pokemon*
-rm -Rf ../src/Controller/*
-rm -Rf ../src/Domain/ReadModel/*
-rm -Rf ../src/Domain/WriteModel/*
+rm -Rf src/Console/Pokemon*
+rm -Rf src/Controller/*
+rm -Rf src/Domain/ReadModel/*
+rm -Rf src/Domain/WriteModel/*
 # Templates folder.
-rm -Rf ../templates/*
+rm -Rf templates/*
 # Tests folder.
-rm -Rf ../tests/Console/Pokemon*
-rm -Rf ../tests/Controller/*
-rm -Rf ../tests/Domain/ReadModel/*
-rm -Rf ../tests/Domain/WriteModel/*
+rm -Rf tests/Console/Pokemon*
+rm -Rf tests/Controller/*
+rm -Rf tests/Domain/ReadModel/*
+rm -Rf tests/Domain/WriteModel/*
 
 
 echo "All examples related files are deleted.
