d543624ac905615892841b92e30f6a3493a98214
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2022-08-14 14:58:06 +0200

    Removed example code

--- config/routes.php
+++ config/routes.php
@@ -1,19 +1,11 @@
+
 <?php
 
-use App\Controller\ChooseCoolestPokemonRequestHandler;
-use App\Controller\ResultRequestHandler;
-use Slim\App;
+  use Slim\App;
 use Slim\Handlers\Strategies\RequestResponseArgs;
 
 return function (App $app) {
     // Set default route strategy.
     $routeCollector = $app->getRouteCollector();
     $routeCollector->setDefaultInvocationStrategy(new RequestResponseArgs());
-
-    $app
-        ->get('/[{previousPokemonUpvotedId}/{previousPokemonNotUpvotedId}]', ChooseCoolestPokemonRequestHandler::class.':handle')
-        ->setName('index');
-    $app
-        ->get('/results', ResultRequestHandler::class.':handle')
-        ->setName('results');
 };
